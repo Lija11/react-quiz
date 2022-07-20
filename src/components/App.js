dada82e4198a3678cff67d361d83cf31872c8342
lija11 <lijasm32@gmail.com>
2022-07-20 12:20:05 +0600

    react-quiz

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>hello word</h1>
+      <h2>hello word</h2>
+      <h2>hello word</h2>
+      <h2>hello word</h2>
+      <h2>hello word</h2>
+      <h2>hello word</h2>
+    </div>
+  );
+}
+
+export default App;
